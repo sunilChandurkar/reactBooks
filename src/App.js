caa0b30fa8cbf25cf235f5ee9bd388893d5ddce7
Sunil Chandurkar <sunil_chandurkar@yahoo.com>
2024-02-18 19:58:30 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+    const [books, setBooks] = useState([]);
+
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books');
+
+        setBooks(response.data);
+    };
+
+    const createBook = async (title) => {
+        const response = await axios.post('http://localhost:3001/books', {
+            title: title
+        });
+
+        
+        const updatedBooks = [
+            ...books,
+            response.data
+        ];
+        setBooks(updatedBooks);
+    };
+
+    const editBookById = (id, newTitle) => {
+        const updatedBooks = books.map((book) => {
+            if(book.id === id){
+                return {...book, title: newTitle};
+            }
+            return book;
+        });
+        setBooks(updatedBooks);
+    };
+
+    const deleteBookById = (id) => {
+        const updatedBooks = books.filter((book) => {
+            return book.id !== id;
+        });
+        setBooks(updatedBooks);
+    };
+
+    return <div className="app">
+        <h1>Reading List</h1>
+        <BookList onEdit={editBookById} books={books} onDelete={deleteBookById} />
+        <BookCreate onCreate={createBook}/>
+    </div>;
+} 
 
 export default App;
